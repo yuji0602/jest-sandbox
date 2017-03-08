4043aa7981b89396fdf50986c00287fc7d4ee0e6
yuji0602 <eto@cocolable.co.jp>
2017-03-08 10:29:34 +0900

    describeを追加

--- test/test.js
+++ test/test.js
@@ -1,88 +1,90 @@
 'use strict';
 
-// Common Matchers
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4);
-});
+describe('basic test', () => {
+  // Common Matchers
+  test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+  });
 
-test('object assignment', () => {
-  let data = {one: 1};
-  data['two'] = 2;
-  expect(data).toEqual({one: 1, two: 2});
-});
+  test('object assignment', () => {
+    let data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+  });
 
-test('adding positive numbers is not zero', () => {
-  for (let a = 1; a < 10; a++) {
-    for (let b = 1; b < 10; b++) {
-      expect(a + b).not.toBe(0);
+  test('adding positive numbers is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+      for (let b = 1; b < 10; b++) {
+        expect(a + b).not.toBe(0);
+      }
     }
-  }
-});
+  });
 
-// Truthiness
-test('null', () => {
-  let n = null;
-  expect(n).toBeNull();
-  expect(n).toBeDefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
-});
+  // Truthiness
+  test('null', () => {
+    let n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
 
-test('zero', () => {
-  let z = 0;
-  expect(z).not.toBeNull();
-  expect(z).toBeDefined();
-  expect(z).not.toBeUndefined();
-  expect(z).not.toBeTruthy();
-  expect(z).toBeFalsy();
-})
+  test('zero', () => {
+    let z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+  })
 
-// Numbers
-test('two plus two', () => {
-  let value = 2 + 2;
-  expect(value).toBeGreaterThan(3);
-  expect(value).toBeGreaterThanOrEqual(3.5);
-  expect(value).toBeLessThan(5);
-  expect(value).toBeLessThanOrEqual(4.5);
+  // Numbers
+  test('two plus two', () => {
+    let value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
 
-  // toBe and toEqual are equivalent for numbers
-  expect(value).toBe(4);
-  expect(value).toEqual(4);
-});
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+  });
 
-test('adding floating point numbers', () => {
-  let value = 0.1 + 0.2;
-  expect(value).not.toBe(0.3);    // It isn't! Because rounding error
-  expect(value).toBeCloseTo(0.3); // This works.
-});
+  test('adding floating point numbers', () => {
+    let value = 0.1 + 0.2;
+    expect(value).not.toBe(0.3);    // It isn't! Because rounding error
+    expect(value).toBeCloseTo(0.3); // This works.
+  });
 
-// Strings
-test('there is no I in team', () => {
-  expect('team').not.toMatch(/I/);
-});
+  // Strings
+  test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+  });
 
-test('but there is a "stop" in Christoph', () => {
-  expect('Christoph').toMatch(/stop/);
-});
+  test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+  });
 
-// Arrays
-test('the shopping list has beer on it', () => {
-  let shoppingList = ['diapers', 'kleenex', 'trash bags', 'paper towels', 'beer'];
+  // Arrays
+  test('the shopping list has beer on it', () => {
+    let shoppingList = ['diapers', 'kleenex', 'trash bags', 'paper towels', 'beer'];
 
-  expect(shoppingList).toContain('beer');
-});
+    expect(shoppingList).toContain('beer');
+  });
 
-// Exceptions
-test('compiling android goes as expected', () => {
-  function compileAndroidCode() {
-    throw new Error('you are using the wrong JDK');
-  }
+  // Exceptions
+  test('compiling android goes as expected', () => {
+    function compileAndroidCode() {
+      throw new Error('you are using the wrong JDK');
+    }
 
-  expect(compileAndroidCode).toThrow();
-  expect(compileAndroidCode).toThrow(Error);
+    expect(compileAndroidCode).toThrow();
+    expect(compileAndroidCode).toThrow(Error);
 
-  // You can also use the exact error message or a regexp
-  expect(compileAndroidCode).toThrow('you are using the wrong JDK');
-  expect(compileAndroidCode).toThrow(/JDK/);
+    // You can also use the exact error message or a regexp
+    expect(compileAndroidCode).toThrow('you are using the wrong JDK');
+    expect(compileAndroidCode).toThrow(/JDK/);
+  });
 });
